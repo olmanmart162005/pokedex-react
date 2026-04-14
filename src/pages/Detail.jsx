1ed1c6e677a79153431a5a1a86050e2300a9b5dd
olmanmart162005 <olme_martinezm56@unicah.edu>
2026-04-13 23:11:13 -0600

    fix detail async

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,21 +1,41 @@
 import { useParams } from "react-router";
 import { usePokemonService } from "../context/Context";
-export const Detail = ()=>{
-    const { id } = useParams();
-    const pokemonServiceInstance = usePokemonService();
-    const pokemonData = pokemonServiceInstance.GetPokemonById(id);
-    return (
-        <section>
-            <h1>Detail Page</h1>
-            <p> Pokemon ID:  { id }</p>
-            <div>
-                PokemonDetail
-            </div>
-            <pre>
-                {JSON.stringify(pokemonData, null, 2)}
-            </pre>
-        </section>
-    )
-}
+import { useEffect, useState } from "react";
+
+export const Detail = () => {
+  const { id } = useParams();
+  const pokemonServiceInstance = usePokemonService();
+
+  const [pokemonData, setPokemonData] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await pokemonServiceInstance.GetPokemonById(id);
+      setPokemonData(data);
+    };
+
+    getData();
+  }, [id]);
+
+  // 🔥 IMPORTANTE
+  if (!pokemonData) {
+    return <h1 className="text-white text-center mt-10">Cargando...</h1>;
+  }
+
+  return (
+    <section>
+      <h1>Detail Page</h1>
+      <p>Pokemon ID: {id}</p>
+
+      <div>
+        PokemonDetail
+      </div>
+
+      <pre>
+        {JSON.stringify(pokemonData, null, 2)}
+      </pre>
+    </section>
+  );
+};
 
 export default Detail;
