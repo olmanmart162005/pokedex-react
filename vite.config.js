529eb30d7884d8b42d90721b8062d158929f668e
olmanmart162005 <olme_martinezm56@unicah.edu>
2026-04-13 22:58:09 -0600

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.DEPLOY_BASE_URL ?? '/' ,
+  base: '/pokedex-react/', // 👈 ESTE ES EL FIX
   plugins: [
     react(),
     tailwindcss()
